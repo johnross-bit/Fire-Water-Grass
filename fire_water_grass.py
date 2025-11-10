143f458ad2776a2a8410d089fcdbd6afce1351f2
johnross-bit <johnross@midsouthgiftedacademy.com>
2025-11-10 13:54:09 -0600

    Add files via upload

---
+++ fire_water_grass.py
@@ -0,0 +1,49 @@
+
+
+def fire_water_grass(choice_1, choice_2, player_count):
+    if player_count == ("one"):    
+        if choice_2 == 1 and choice_1 == 3:
+            print("You lose, Fire beats Grass.")
+            return 2
+        if choice_2 == 2 and choice_1 == 1:
+            print("You lose, Water beats Fire.")
+            return 2.1
+        if choice_2 == 3 and choice_1 == 2:
+            print("You lose, Grass beats Water.")
+            return 2.2
+        if choice_1 == 1 and choice_2 == 3:
+            print("You win, Fire beats Grass!")
+            return 1
+        elif choice_1 == 2 and choice_2 == 1:
+            print("You win, Water beats Fire!")
+            return 1.1
+        elif choice_1 == 3 and choice_2 == 2:
+            print("You win, Grass beats Water!")
+            return 1.2
+        if choice_1 == choice_2:
+            print("It was a tie!")
+            return 3
+    elif player_count == "two":
+        if choice_2 == 1 and choice_1 == 3:
+            print("Player 2 wins, Fire beats Grass.")
+            return 2
+        if choice_2 == 2 and choice_1 == 1:
+            print("Player 2 wins, Water beats Fire.")
+            return 2.1
+        if choice_2 == 3 and choice_1 == 2:
+            print("Player 2 wins, Grass beats Water.")
+            return 2.2
+        if choice_1 == 1 and choice_2 == 3:
+            print("Player 1 wins, Fire beats Grass!")
+            return 1
+        elif choice_1 == 2 and choice_2 == 1:
+            print("Player 1 wins, Water beats Fire!")
+            return 1.1
+        elif choice_1 == 3 and choice_2 == 2:
+            print("Player 1 wins, Grass beats Water!")
+            return 1.2
+        if choice_1 == choice_2:
+            print("It was a tie!")
+            return 3
+
+
---
+++ main.py
@@ -0,0 +1,69 @@
+import random
+from fire_water_grass import fire_water_grass
+
+y = 0
+a = 0
+b = 0
+c = 0
+player_count = input("One or two player: ")
+
+while y == 0:
+    while a == 0:   
+        if player_count  == ("one"):
+            choice_2 = random.randint(1,3)
+            choice_1 =input("Choose Fire, Water, or Grass: ")
+    
+            if choice_1 == ("fire"):
+                choice_1 = 1
+            if choice_1 == ("water"):
+                choice_1 == 2
+            if choice_1 == ("grass"):
+                choice_1 = 3
+            
+            win_player = fire_water_grass(choice_1, choice_2, player_count)
+            pl_ag = input("Would You like to play again: ")
+            if pl_ag == "yes":
+                continue
+            y = y + 1
+            a = a + 1
+            #break
+            #print("stuck in a loop")
+        elif player_count  == ("two"):
+            choice_1 = input("Player 1 choose Fire, Water, or Grass: ")
+
+            x = 0
+            while x < 50:
+                print("                                                  ")
+                x = x + 1
+            choice_2 = input("Player 2 choose Fire, Water, or Grass: ")
+
+            if choice_1 == ("fire"):
+                choice_1 = 1
+            if choice_2 == ("fire"):
+                choice_2 = 1
+            if choice_1 == ("water"):
+                choice_1 = 2
+            if choice_2 == ("water"):
+                choice_2 = 2
+            if choice_1 == ("grass"):
+                choice_1 = 3
+            if choice_2 == ("grass"):
+                choice_2 = 3
+
+               
+            win_player = fire_water_grass(choice_1, choice_2, player_count)
+            pl_ag = input("Would you like to play again: ")
+            if pl_ag == "yes":
+                    continue
+            y = 1
+            a = 1
+            quit
+            
+            
+            
+        
+            
+
+    quit
+quit
+    
