c85b11735868d36865d1e0c55192c7bec4dfdcb0
John Ross <johnross@midsouthgiftedacademy.com>
2025-11-12 08:04:31 -0600

    Add files via upload
    updated version

--- fire_water_grass.py
+++ fire_water_grass.py
@@ -14,15 +14,22 @@ def fire_water_grass(choice_1, choice_2, player_count):
         if choice_1 == 1 and choice_2 == 3:
             print("You win, Fire beats Grass!")
             return 1
-        elif choice_1 == 2 and choice_2 == 1:
+        if choice_1 == 2 and choice_2 == 1:
+            print("second")
             print("You win, Water beats Fire!")
             return 1.1
-        elif choice_1 == 3 and choice_2 == 2:
+        if choice_1 == 3 and choice_2 == 2:
             print("You win, Grass beats Water!")
             return 1.2
-        if choice_1 == choice_2:
-            print("It was a tie!")
+        if choice_1 == 1 and choice_2 == 1:
+            print("It was a tie, you both chose Fire!")
             return 3
+        if choice_1 == 2 and choice_2 == 2:
+            print("It was a tie, you both chose Water!")
+            return 3.1
+        if choice_1 == 3 and choice_2 == 3:
+            print("It was a tie, you both chose Grass!")
+            return 3.2
     elif player_count == "two":
         if choice_2 == 1 and choice_1 == 3:
             print("Player 2 wins, Fire beats Grass.")
@@ -42,8 +49,14 @@ def fire_water_grass(choice_1, choice_2, player_count):
         elif choice_1 == 3 and choice_2 == 2:
             print("Player 1 wins, Grass beats Water!")
             return 1.2
-        if choice_1 == choice_2:
-            print("It was a tie!")
+        if choice_1 == 1 and choice_2 == 1:
+            print("It was a tie, you both chose Fire!")
             return 3
+        if choice_1 == 2 and choice_2 == 2:
+            print("It was a tie, you both chose Water!")
+            return 3.1
+        if choice_1 == 3 and choice_2 == 3:
+            print("It was a tie, you both chose Grass!")
+            return 3.2
 
 
--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import random
 from fire_water_grass import fire_water_grass
+import pygame
+
 
 y = 0
 a = 0
@@ -9,14 +11,14 @@ player_count = input("One or two player: ")
 
 while y == 0:
     while a == 0:   
-        if player_count  == ("one") or ("1") or ("One"):
+        if player_count  == ("one"):
             choice_2 = random.randint(1,3)
             choice_1 =input("Choose Fire, Water, or Grass: ")
     
             if choice_1 == ("fire"):
                 choice_1 = 1
             if choice_1 == ("water"):
-                choice_1 == 2
+                choice_1 = 2
             if choice_1 == ("grass"):
                 choice_1 = 3
             
@@ -28,7 +30,7 @@ while y == 0:
             a = a + 1
             #break
             #print("stuck in a loop")
-        elif player_count  == ("two") or ("2") or ("Two"):
+        elif player_count  == ("two"):
             choice_1 = input("Player 1 choose Fire, Water, or Grass: ")
 
             x = 0
@@ -50,20 +52,71 @@ while y == 0:
             if choice_2 == ("grass"):
                 choice_2 = 3
 
-               
             win_player = fire_water_grass(choice_1, choice_2, player_count)
             pl_ag = input("Would you like to play again: ")
             if pl_ag == "yes":
                     continue
             y = 1
             a = 1
-            quit
-            
+
+if win_player == 2 or win_player == 1 or win_player == 3:  
+    screen_width = 800
+    screen_height = 600
+    screen = pygame.display.set_mode((screen_width, screen_height))
+    pygame.display.set_caption("fire_type_symbol.png")
+    image = pygame.image.load("fire_type_symbol.png").convert_alpha()
+    image = pygame.transform.smoothscale(image, (200, 150))
+
+    running = True
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+        screen.fill((255, 255, 255))
+        screen.blit(image, (300, 200))
+        pygame.display.flip()
+    pygame.quit
+if win_player == 2.1 or win_player == 1.1 or win_player == 3.1:
+    screen_width = 800
+    screen_height = 600
+    screen = pygame.display.set_mode((screen_width, screen_height))
+    pygame.display.set_caption("water_type_symbol.png")
+    image = pygame.image.load("water_type_symbol.png").convert_alpha()
+    image = pygame.transform.smoothscale(image, (200, 150))
+
+    running = True
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+        screen.fill((255, 255, 255))
+        screen.blit(image, (300, 200))
+        pygame.display.flip()
+    pygame.quit
+if win_player == 2.2 or win_player == 1.2 or win_player == 3.2:
+    screen_width = 800
+    screen_height = 600
+    screen = pygame.display.set_mode((screen_width, screen_height))
+    pygame.display.set_caption("grass_type_symbol.jpeg")
+    image = pygame.image.load("grass_type_symbol.jpeg").convert_alpha()
+    image = pygame.transform.smoothscale(image, (200, 150))
+
+    running = True
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+        screen.fill((255, 255, 255))
+        screen.blit(image, (300, 200))
+        pygame.display.flip()
+    pygame.quit
+
+               
             
             
         
             
 
-    quit
+quit
 quit
     
